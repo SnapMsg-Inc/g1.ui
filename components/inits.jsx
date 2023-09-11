89728b8c50948644c509bbd3d04fd823cb8b6256
EduardoCh93 <joseduardoc93@gmail.com>
2023-09-11 19:33:03 -0300

    Add Init App Structure to init app

--- components/inits.jsx
+++ components/inits.jsx
@@ -1,16 +1,63 @@
-import React from "react";
+import {React, useState} from "react";
 import { StatusBar } from "expo-status-bar";
-import {View, Text} from "react-native"
+import {View, Text, TouchableHighlight, Button} from "react-native"
 import styles from "../styles/inits";
 
 function Inits() {
+    const [hidden, setHidden] = useState(false)
+    const Separator = () => <View style={styles.separator} />;
+
     return (
         <>
-            <View style={styles.container}>
-                <Text style={styles.font}>SnapMsg!</Text>
+            <View style={styles.containerLogo}>
+                <Text style={styles.fontLogo}>SnapMsg</Text>
             </View>
-            <View>
-                
+            <View style={styles.container}>
+                <Text style={styles.font}>
+                    Welcome back! 
+                    Log in to see
+                    the latest.
+                </Text>
+            </View>
+            <View style={styles.containerBtn}>
+                <TouchableHighlight>
+                    <View style={styles.btn}>
+                        <Text style={styles.fontBtn}>
+                            Sign in with Google
+                        </Text>
+                    </View>
+                </TouchableHighlight>
+                <TouchableHighlight>
+                    <View style={styles.btn}>
+                        <Text style={styles.fontBtn}>
+                            Sign in with Github
+                        </Text>
+                    </View>
+                </TouchableHighlight>
+                <View style={styles.lineContainer}>
+                    <View style={styles.dash} />
+                    <Text style={styles.text}>or</Text>
+                    <View style={styles.dash} />
+                </View>
+                <TouchableHighlight>
+                    <View style={styles.btnSignUp}>
+                        <Text style={styles.fontSignUp}>
+                            Sign up
+                        </Text>
+                    </View>
+                </TouchableHighlight>
+                <View>
+                    <Text style={styles.fontSugestion}>
+                        Have an account already?
+                    </Text>
+                    <TouchableHighlight>
+                        <View style={styles.btnSignIn}>
+                            <Text style={styles.fontSignIn}>
+                                Sign in
+                            </Text>
+                        </View>
+                    </TouchableHighlight>
+                </View>
             </View>
         </>
     )
--- styles/inits.js
+++ styles/inits.js
@@ -1,16 +1,114 @@
 import { StyleSheet } from "react-native";
 
+const colorBackground = '#000'
+const colorApp = '#1ed760'
+const colorWhite = '#fff'
+const colorGray = '#535353'
+
 const styles = StyleSheet.create({
     container: {
       flex: 1,
-      backgroundColor: '#000',
+      backgroundColor: colorBackground,
+      alignItems:'center',
+    },
+    containerBtn: {
+      flex: 2,
+      backgroundColor: colorBackground,
+    },
+    containerLogo: {
+      flex: 1.6,
+      backgroundColor: colorBackground,
       alignItems: 'center',
       justifyContent: 'center',
     },
-    font: {
-      color: '#1ed760',
+    fontLogo: {
+      color: colorApp,
       fontSize: 46,
     },
-});
-
+    font: {
+      color: colorWhite,
+      fontSize: 40,
+      justifyContent: 'flex-start',
+      marginHorizontal: 40,
+    },
+    btn :{
+      backgroundColor: colorWhite,
+      alignItems:'flex-end',
+      justifyContent: 'center',
+      marginHorizontal: 30,
+      paddingHorizontal: 10,
+      paddingVertical: 5,
+      borderRadius: 20,
+      marginBottom: 20,
+    },
+    fontBtn: {
+      fontSize: 16,
+      fontFamily: 'Roboto',
+      justifyContent: 'flex-end',
+    },
+    lineContainer: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      backgroundColor: colorBackground,
+      paddingHorizontal:30,
+      paddingBottom: 10,
+      marginTop:-10,
+    },
+    dash: {
+      flex: 1,
+      height: 1,
+      backgroundColor: colorGray,
+      marginHorizontal: 1,
+    },
+    text: {
+      fontSize: 16,
+      fontFamily: 'Roboto',
+      marginHorizontal: 10,
+      color: colorGray,
+    },
+    btnSignUp: {
+      backgroundColor: colorApp,
+      alignItems:'center',
+      justifyContent: 'center',
+      marginHorizontal: 30,
+      paddingHorizontal: 10,
+      paddingVertical: 5,
+      borderRadius: 20,
+      marginBottom: 20,
+    },
+    fontSignUp: {
+      fontSize: 24,
+      fontFamily: 'Roboto',
+      justifyContent: 'flex-end',
+      color: colorWhite
+    },
+    btnSignIn: {
+      backgroundColor: colorGray,
+      alignItems:'center',
+      justifyContent: 'center',
+      marginHorizontal: 30,
+      paddingHorizontal: 10,
+      paddingVertical: 5,
+      borderRadius: 20,
+      marginBottom: 20,
+      borderWidth: 1,
+      borderColor: colorApp,
+    },
+    fontSignIn: {
+      fontSize: 24,
+      fontFamily: 'Roboto',
+      justifyContent: 'flex-end',
+      color: colorApp
+    },
+    fontSugestion: {
+      fontSize: 14,
+      fontFamily: 'Roboto',
+      marginHorizontal: 10,
+      color: colorWhite,
+      marginHorizontal: 30,
+      marginBottom: 3,
+      marginTop: 20
+    }
+  });
+  
 export default styles
