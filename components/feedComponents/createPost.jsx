a96e5c5a8d77d280e95e96d99a8ffcbf4643c1f8
mdavic0 <mdavico@fi.uba.ar>
2023-10-27 04:57:30 -0300

    feat: create post screen
    without funcionality

--- components/feedComponents/createPost.jsx
+++ components/feedComponents/createPost.jsx
@@ -1,12 +1,17 @@
 import React, { useEffect, useState, useContext } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Image, TextInput, TouchableHighlight } from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
 import { FloatingAction } from "react-native-floating-action";
 import { Octicons } from '@expo/vector-icons';
 import SimpleLineIcons from 'react-native-vector-icons/SimpleLineIcons';
 import EvilIcons from 'react-native-vector-icons/EvilIcons';
+import { LoggedUserContext } from '../connectivity/auth/loggedUserContext';
+import BackButton from '../buttons/buttonBack';
 
 const CreatePostScreen = ({ navigation }) => {
+    const { userData } = useContext(LoggedUserContext)
+    const [text, setText] = useState('');
+
     const takePhotoFromCamera = () => {
         console.log("take photo from camera")
     };
@@ -35,14 +40,44 @@ const CreatePostScreen = ({ navigation }) => {
     ];
 	return (
 		<View style={styles.container}>
-			<Text style={styles.text}> CREATE POST </Text>
+            <View style={styles.header}>
+                {/* Back button */}
+                <BackButton onPress={() => {navigation.goBack()}}/>
+                <TouchableHighlight
+                        style={styles.cancelButton}
+                        onPress={() => { 
+                            navigation.goBack()
+                        }}
+                    >
+                        <Text style={styles.cancelButtonLabel}>Cancel</Text>
+                </TouchableHighlight>
+                <TouchableHighlight
+                    style={styles.postButton}
+                    onPress={() => { }}>
+                    <Text style={styles.postButtonLabel}>Post</Text>
+                </TouchableHighlight>
+            </View>
+
+            <View style={styles.inputContainer}>
+                <Image source={{uri: userData.pic}} style={styles.image} />
+                <TextInput
+                    value={text}
+                    onChangeText={setText}
+                    placeholder="What's happening?"
+                    multiline
+                    numberOfLines={5}
+                    style={styles.textInput}
+                    placeholderTextColor="white"
+                    autoFocus
+                />
+            </View>
             <FloatingAction
                 actions={actions}
                 onPressItem={name => {
                 console.log(`selected button: ${name}`);
                 }}
                 color={colorApp}
-                overlayColor= 'rgba(68, 68, 68, 0.3)'
+                overlayColor= 'rgba(68, 68, 68, 0.0)'
                 iconHeight={30}
                 iconWidth={30} 
                 floatingIcon={(
@@ -61,18 +96,67 @@ const styles = StyleSheet.create({
 	container: {
 		flex: 1,
 		backgroundColor: 'black',
-		alignItems: 'center',
-        justifyContent: 'center',
+        padding: 10,
 	},
+    header: {
+        flexDirection: 'row',
+        alignItems: 'flex-end',
+        backgroundColor: 'black',
+        marginBottom: 10,
+        borderBottomWidth: 1, // Agrega un borde inferior de 1 píxel
+        borderBottomColor: 'rgba(255, 255, 255, 0.3)', // Color del borde
+        height: 50,
+    },
     text: {
 		color: colorText,
 		paddingVertical: 10,
 	},
+    inputContainer: {
+        flexDirection: 'row',
+    },
+    image: {
+        width: 50,
+        aspectRatio: 1,
+        borderRadius: 50,
+        marginRight: 10,
+    },
     actionButtonIcon: {
         fontSize: 20,
         height: 22,
         color: 'white',
-      },
+    },
+    postButton: {
+        position: 'absolute',
+        right: 10,
+        top: 10,
+        borderWidth: 1,
+        borderColor: colorApp,
+        borderRadius: 15,
+		justifyContent: 'center',
+		alignItems: 'center',
+		width: 110,
+		height: 30,
+		paddingHorizontal: 15,
+    },
+    postButtonLabel: {
+        color: colorApp,
+        fontSize: 16,
+        fontWeight: 'bold',
+    },
+    textInput: {
+        flex: 1,
+        color: 'white',
+        height: 50,
+    },
+	cancelButton: {
+		position: 'absolute',
+        left: 50,
+        top: 12,
+	},
+    cancelButtonLabel: {
+        color: 'white',
+		fontSize: 16,
+    }
 });
 
 export default CreatePostScreen;
