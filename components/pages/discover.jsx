37a1bc9225ea1b5e8e1dd662d54bb24b86e342a8
mdavic0 <mdavico@fi.uba.ar>
2023-11-09 12:50:19 -0300

    refactor: separate Discover styles from code

--- components/pages/discover.jsx
+++ components/pages/discover.jsx
@@ -8,14 +8,16 @@ import { useFocusEffect } from '@react-navigation/native';
 import { LoggedUserContext } from '../connectivity/auth/loggedUserContext';
 import { useRoute } from '@react-navigation/native';
 import PostButton from '../buttons/buttonPost';
+import { colorApp, colorText, colorBackground } from '../../styles/appColors/appColors';
+import styles from '../../styles/discover/discover';
 
 const tabBar = props => (
 	<MaterialTabBar
 		{...props}
-		indicatorStyle={{ backgroundColor: '#1ed760', height: 3, }}
+		indicatorStyle={{ backgroundColor: colorApp, height: 3, }}
 		style={styles.tabBar}
-		activeColor='#1ed760'
-		inactiveColor='#535353' 
+		activeColor={colorApp}
+		inactiveColor={colorText} 
 		labelStyle={styles.label}
 	/>
 );
@@ -55,51 +57,3 @@ export default function Discover({ navigation }) {
 		</View>   
 	);
 };
-
-const colorBackground = '#000'
-const colorApp = '#1ed760'
-export const colorText = '#535353'
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        flexDirection: 'column',
-        backgroundColor: colorBackground,
-        justifyContent: 'center',
-    },
-    header: {
-        justifyContent: 'space-between',
-        alignItems:'center',
-        flexDirection:'row',
-        paddingVertical:10,
-        borderWidth: 1,
-        borderColor: colorApp,
-        borderRadius: 10,
-        marginHorizontal: 10,
-        paddingHorizontal: 10,
-    },
-    containerLogo: {
-        backgroundColor: 'transparent',
-        alignItems: 'center',
-        flexDirection: 'row',
-        justifyContent: 'center',
-    },
-    fontLogo: {
-        color: colorApp,
-        fontSize: 25,
-        marginLeft: 20,
-    },
-    font: {
-        color: colorApp,
-        fontSize: 18,
-        marginLeft: 5,
-        fontWeight: 'bold'
-    },
-    searchBox: {
-        paddingHorizontal: 20,
-        
-    },
-    tabBar: {
-		backgroundColor: 'black',
-	},
-})
