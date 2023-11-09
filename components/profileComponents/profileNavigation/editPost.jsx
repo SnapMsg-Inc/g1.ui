08f4393d6b0cf1abb5dac4a1a99113c584bcfe9f
mdavic0 <mdavico@fi.uba.ar>
2023-11-09 14:01:00 -0300

    refactor: separate EditPost styles from code

--- components/profileComponents/profileNavigation/editPost.jsx
+++ components/profileComponents/profileNavigation/editPost.jsx
@@ -14,6 +14,8 @@ import { PatchPostData, createPost } from '../../connectivity/servicesUser';
 import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 import { useRoute } from '@react-navigation/native';
+import { colorApp, colorBackground, colorText } from '../../../styles/appColors/appColors';
+import styles from '../../../styles/profile/editPost';
 
 const EditPost = ({ navigation }) => {
     const route = useRoute();
@@ -260,87 +262,5 @@ const EditPost = ({ navigation }) => {
 		</View>
 	);
 };
-
-const colorBackground = '#000'
-const colorApp = '#1ed760'
-export const colorText = '#535353'
-
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: 'black',
-        padding: 10,
-	},
-    header: {
-        flexDirection: 'row',
-        alignItems: 'flex-end',
-        backgroundColor: 'black',
-        marginBottom: 10,
-        borderBottomWidth: 1,
-        borderBottomColor: 'rgba(255, 255, 255, 0.3)',
-        height: 50,
-    },
-    text: {
-		color: colorText,
-		paddingVertical: 10,
-	},
-    inputContainer: {
-        flex: 1,
-        flexDirection: 'row',
-    },
-    image: {
-        width: 50,
-        aspectRatio: 1,
-        borderRadius: 50,
-        marginRight: 10,
-    },
-    actionButtonIcon: {
-        fontSize: 20,
-        height: 22,
-        color: 'white',
-    },
-    postButton: {
-        position: 'absolute',
-        right: 10,
-        top: 10,
-        borderWidth: 1,
-        borderColor: colorApp,
-        borderRadius: 15,
-		justifyContent: 'center',
-		alignItems: 'center',
-		height: 30,
-		paddingHorizontal: 15,
-    },
-    postButtonLabel: {
-        color: colorApp,
-        fontSize: 16,
-        fontWeight: 'bold',
-    },
-    textInput: {
-        flex: 1,
-        color: 'white',
-        textAlignVertical: 'top',
-    },
-	cancelButton: {
-		position: 'absolute',
-        left: 50,
-        top: 12,
-	},
-    cancelButtonLabel: {
-        color: 'white',
-		fontSize: 16,
-    },
-    postImage: {
-        borderRadius: 15,
-        width: '%100',
-        height: 250,
-        marginVertical: 10,
-    },
-    statusWrapper: {
-        position: 'absolute',
-        right: 10,
-        top: -15,
-    }
-});
 
 export default EditPost;
