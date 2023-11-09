c6ee527b62ce698c4fcb7531e664ffc12d22a9d4
mdavic0 <mdavico@fi.uba.ar>
2023-11-09 11:35:27 -0300

    refactor: separate CreatePost styles from code

--- components/common/createPost.jsx
+++ components/common/createPost.jsx
@@ -13,6 +13,8 @@ import storage from '@react-native-firebase/storage';
 import { createPost } from '../connectivity/servicesUser';
 import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
 import { TouchableOpacity } from 'react-native-gesture-handler';
+import styles from '../../styles/common/createPost';
+import { colorApp, colorBackground, colorText } from '../../styles/appColors/appColors';
 
 const CreatePostScreen = ({ navigation }) => {
     const { userData } = useContext(LoggedUserContext)
@@ -141,10 +143,6 @@ const CreatePostScreen = ({ navigation }) => {
         setUploading(false);
         setImage(null);
     
-        // Alert.alert(
-        //   'Image uploaded!',
-        //   'Your image has been uploaded to the Firebase Cloud Storage Successfully!',
-        // );
         return url;
     
         } catch (e) {
@@ -265,88 +263,5 @@ const CreatePostScreen = ({ navigation }) => {
 		</View>
 	);
 };
-
-const colorBackground = '#000'
-const colorApp = '#1ed760'
-export const colorText = '#535353'
-
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: 'black',
-        padding: 10,
-	},
-    header: {
-        flexDirection: 'row',
-        alignItems: 'flex-end',
-        backgroundColor: 'black',
-        marginBottom: 10,
-        borderBottomWidth: 1,
-        borderBottomColor: 'rgba(255, 255, 255, 0.3)',
-        height: 50,
-    },
-    text: {
-		color: colorText,
-		paddingVertical: 10,
-	},
-    inputContainer: {
-        flex: 1,
-        flexDirection: 'row',
-    },
-    image: {
-        width: 50,
-        aspectRatio: 1,
-        borderRadius: 50,
-        marginRight: 10,
-    },
-    actionButtonIcon: {
-        fontSize: 20,
-        height: 22,
-        color: 'white',
-    },
-    postButton: {
-        position: 'absolute',
-        right: 10,
-        top: 10,
-        borderWidth: 1,
-        borderColor: colorApp,
-        borderRadius: 15,
-		justifyContent: 'center',
-		alignItems: 'center',
-		width: 110,
-		height: 30,
-		paddingHorizontal: 15,
-    },
-    postButtonLabel: {
-        color: colorApp,
-        fontSize: 16,
-        fontWeight: 'bold',
-    },
-    textInput: {
-        flex: 1,
-        color: 'white',
-        textAlignVertical: 'top',
-    },
-	cancelButton: {
-		position: 'absolute',
-        left: 50,
-        top: 12,
-	},
-    cancelButtonLabel: {
-        color: 'white',
-		fontSize: 16,
-    },
-    postImage: {
-        borderRadius: 15,
-        width: '%100',
-        height: 250,
-        marginVertical: 10,
-    },
-    statusWrapper: {
-        position: 'absolute',
-        right: 10,
-        top: -15,
-    }
-});
 
 export default CreatePostScreen;
---
+++ styles/common/createPost.js
@@ -0,0 +1,84 @@
+import { StyleSheet } from "react-native";
+import { colorBackground, colorText, colorApp } from "../appColors/appColors";
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		backgroundColor: 'black',
+        padding: 10,
+	},
+    header: {
+        flexDirection: 'row',
+        alignItems: 'flex-end',
+        backgroundColor: 'black',
+        marginBottom: 10,
+        borderBottomWidth: 1,
+        borderBottomColor: 'rgba(255, 255, 255, 0.3)',
+        height: 50,
+    },
+    text: {
+		color: colorText,
+		paddingVertical: 10,
+	},
+    inputContainer: {
+        flex: 1,
+        flexDirection: 'row',
+    },
+    image: {
+        width: 50,
+        aspectRatio: 1,
+        borderRadius: 50,
+        marginRight: 10,
+    },
+    actionButtonIcon: {
+        fontSize: 20,
+        height: 22,
+        color: 'white',
+    },
+    postButton: {
+        position: 'absolute',
+        right: 10,
+        top: 10,
+        borderWidth: 1,
+        borderColor: colorApp,
+        borderRadius: 15,
+		justifyContent: 'center',
+		alignItems: 'center',
+		width: 110,
+		height: 30,
+		paddingHorizontal: 15,
+    },
+    postButtonLabel: {
+        color: colorApp,
+        fontSize: 16,
+        fontWeight: 'bold',
+    },
+    textInput: {
+        flex: 1,
+        color: 'white',
+        textAlignVertical: 'top',
+    },
+	cancelButton: {
+		position: 'absolute',
+        left: 50,
+        top: 12,
+	},
+    cancelButtonLabel: {
+        color: 'white',
+		fontSize: 16,
+    },
+    postImage: {
+        borderRadius: 15,
+        width: '%100',
+        height: 250,
+        marginVertical: 10,
+    },
+    statusWrapper: {
+        position: 'absolute',
+        right: 10,
+        top: -15,
+    }
+});
+
+export default styles;
+
