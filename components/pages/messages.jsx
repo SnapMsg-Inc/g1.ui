90e673291e3316b83b90f9d15e548250beebc92f
mdavic0 <mdavico@fi.uba.ar>
2023-11-18 00:24:55 -0300

    feat: messages header

--- components/pages/messages.jsx
+++ components/pages/messages.jsx
@@ -1,29 +1,45 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import {
     StyleSheet,
     View,
     Text,
-} from 'react-native';
+    Pressable,
+    TouchableOpacity,
+    ActivityIndicator,
+    RefreshControl,
+    FlatList } from 'react-native';
+import { useFocusEffect } from '@react-navigation/native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import { Octicons } from '@expo/vector-icons';
+import { ScrollView, TouchableHighlight } from 'react-native-gesture-handler';
+import { GetFeedPosts, GetPosts, GetUserData } from '../connectivity/servicesUser';
+import { DrawerActions, CommonActions } from '@react-navigation/native';
+import PostButton from '../buttons/buttonPost';
+import styles from '../../styles/feed/feed';
+import { colorApp, colorText, colorBackground } from '../../styles/appColors/appColors';
+import SnapMsg from '../common/SnapMsg';
+import { FontAwesome5 } from 'react-native-vector-icons';
 
 export default function Messages({ navigation }) {
     return (
-        <View style={styles.body}>
-            <Text style={styles.text}>
-                Messages
-            </Text>
+        <View style={styles.container}>
+            <View style={styles.header}>
+                <TouchableHighlight
+                    onPress={() => {
+                        navigation.dispatch(DrawerActions.openDrawer());
+                    }}
+                >
+                    <View style={{ flexDirection: 'row', justifyContent: 'center' }}>
+                        <FontAwesome5 name="envelope" color={colorApp} size={28} />
+                        <Text style={styles.font}>Messages</Text>
+                    </View>
+                </TouchableHighlight>
+                <View style={styles.containerLogo}>
+                    <Icon name="snapchat-ghost" color={colorApp} size={30} />
+                    <Icon name="envelope" color={colorApp} size={10} />
+                </View>
+            </View>
+            <PostButton onPress={() => navigation.navigate('CreatePostScreen')} />
         </View>
     )
 }
-
-const styles = StyleSheet.create({
-    body: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    text: {
-        fontSize: 40,
-        fontWeight: 'bold',
-        margin: 10,
-    }
-})
